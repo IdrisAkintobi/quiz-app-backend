dda2e70cc6e981dbb73ea4f5e1eb3bbe11485f63
Idris Akintobi <akintobiidris@gmail.com>
2022-08-13 14:37:26 +0100

    created server index file

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import app from "./app";
+import connectDB from "./db/connectDB";
+
+const PORT = process.env.NODE_DOCKER_PORT;
+
+const startServer = async () => {
+  await connectDB();
+  app.listen(PORT, () =>
+    console.log(`🚀 Server running on http://localhost:${PORT}`)
+  );
+};
+
+//Start server
+startServer().catch(console.log);
