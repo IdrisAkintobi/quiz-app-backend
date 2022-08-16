3f7ad1e465c39427d3ffb175acd095c488d94f6c
Idris Akintobi <akintobiidris@gmail.com>
2022-08-16 11:08:01 +0100

    created app file

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,20 @@
 import "dotenv/config";
 import morgan from "morgan";
+import helmet from "helmet";
+import user from "./routes/user.auth.route";
+import quiz from "./routes/quiz.route";
 import express, { json } from "express";
+import { expressjwt as jwt } from "express-jwt";
 import pageNotFound from "./middlewares/notFound";
 import errorHandler from "./middlewares/errorHandler";
-import user from "./routes/user.route";
-import { expressjwt as jwt } from "express-jwt";
 
 //Instantiate app server
 const app = express();
 
 //Middlewares
-app.use(json());
+app.use(helmet());
 app.use(morgan("dev"));
+app.use(json());
 app.use(
   jwt({
     secret: process.env.JWT_SECRET as string,
@@ -22,6 +25,7 @@ app.use(
 //Routes
 app.get("/", (_, res) => res.json({ message: "Connected" }));
 app.use("/api/user", user);
+app.use("/api/quiz", quiz);
 
 // Error handlers
 app.use(pageNotFound);
--- src/db/connectDB.ts
+++ src/db/connectDB.ts
@@ -1,22 +1,38 @@
 import mongoose from "mongoose";
+import Redis from "ioredis";
 
 const {
-  MONGO_HOST: host,
-  MONGODB_DOCKER_PORT: port,
-  MONGODB_NAME: name,
-  MONGODB_USER: user,
-  MONGODB_PASSWORD: password,
+  MONGO_HOST: DB_Host,
+  MONGODB_DOCKER_PORT: DB_Port,
+  MONGODB_NAME: DB_Name,
+  MONGODB_USER: DB_User,
+  MONGODB_PASSWORD: DB_Pwd,
+  REDIS_HOST,
+  REDIS_PORT,
+  REDIS_PASSWORD,
 } = process.env;
 
 const connectDB = async () => {
   try {
-    const uri = `mongodb://${user}:${password}@${host}:${port}/${name}?authSource=admin`;
-    await mongoose.connect(uri);
-    console.log(`🔌 Database connected to ${host}`);
+    const atlas_uri = `mongodb+srv://root:${DB_Pwd}@cloud.rcnz6fx.mongodb.net/${DB_Name}?retryWrites=true&w=majority`;
+    const uri = `mongodb://${DB_User}:${DB_Pwd}@${DB_Host}:${DB_Port}/${DB_Name}?replicaSet=rs`;
+    mongoose.connect(uri);
+    console.log(`🔌 Database connected to ${DB_Host}`);
   } catch (err) {
     console.log(`could not connect to mongodb ---- ${err}`);
     process.exit(1);
   }
 };
+
+export const redisClient = new Redis({
+  host: REDIS_HOST,
+  port: (REDIS_PORT as any) || 6379,
+  password: REDIS_PASSWORD,
+});
+
+redisClient.on("connect", () =>
+  console.log(`⚡ Redis connected to ${REDIS_HOST}`)
+);
+redisClient.on("error", (err) => console.log(`🚩 Redis error: ${err}`));
 
 export default connectDB;
