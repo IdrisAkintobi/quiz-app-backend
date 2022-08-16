35a0c6221498b20c592fa6551e3a4b804f98c70e
Idris Akintobi <akintobiidris@gmail.com>
2022-08-16 11:17:18 +0100

    created auth controller

--- src/controllers/user/auth.ts
+++ src/controllers/user/auth.ts
@@ -2,7 +2,10 @@ import { User } from "../../model/user";
 import genToken from "../../utils/gen.token";
 import { Request, Response } from "express";
 import asyncHandler from "express-async-handler";
-import { SignUpValidator, SignInValidator } from "../../utils/validators";
+import {
+  SignUpValidator,
+  SignInValidator,
+} from "../../utils/validators/auth.validator";
 
 const signup = asyncHandler(async (req: Request, res: Response) => {
   const { email, password } = SignUpValidator.parse(req.body);
@@ -11,15 +14,15 @@ const signup = asyncHandler(async (req: Request, res: Response) => {
   res.status(201).json({ message: "User created successfully" });
 });
 
-const login = async (req: Request, res: Response) => {
+const login = asyncHandler(async (req: Request, res: Response) => {
   const { email, password } = SignInValidator.parse(req.body);
   const user = await User.findOne({ email });
-  if (!user || !(await user.validatePassword(password)))
-    return res.status(401).json({ message: "Invalid credentials" });
+  if (!user || !(await user.validatePassword(password))) {
+    res.status(401);
+    throw new Error("Invalid credentials");
+  }
 
-  res
-    .status(200)
-    .json({ message: "User login successfully", token: genToken(user._id) });
-};
+  res.status(200).json({ token: genToken(user._id) });
+});
 
 export { signup, login };
