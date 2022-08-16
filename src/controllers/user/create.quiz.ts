9fda4881d0bb6cb21faefca14bfd9e66bc8ad299
Idris Akintobi <akintobiidris@gmail.com>
2022-08-16 02:15:14 +0100

    moved

--- src/controllers/user/create.quiz.ts
+++
@@ -1,8 +0,0 @@
-import { Request, Response } from "express";
-import asyncHandler from "express-async-handler";
-
-const createQuiz = asyncHandler(async (req: Request, res: Response) => {
-  res.status(200).json({ data: req.auth.email });
-});
-
-export { createQuiz };
