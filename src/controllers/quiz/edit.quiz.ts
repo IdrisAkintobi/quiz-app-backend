2f8e8a4071fc270c57ca042c8966809f0b802ee3
Idris Akintobi Adejare <akintobiidris@gmail.com>
2022-08-16 15:32:28 +0100

    added redis

--- src/controllers/quiz/edit.quiz.ts
+++ src/controllers/quiz/edit.quiz.ts
@@ -3,6 +3,7 @@ import { Quiz } from "../../model/quiz.model";
 import asyncHandler from "express-async-handler";
 import { quizEditValidator } from "../../utils/validators/quiz.edit.validator";
 import { validateUrl } from "../../utils/validators/queryAndParamsValidator";
+import { deleteRedisQuiz } from "../../utils/delete.redis.quiz";
 
 const editQuiz = asyncHandler(async (req: Request, res: Response) => {
   const { id } = validateUrl(req);
@@ -14,10 +15,18 @@ const editQuiz = asyncHandler(async (req: Request, res: Response) => {
     res.status(400);
     throw new Error("Published quiz cannot be edited");
   }
+
   const { title, isPublished } = quizEditValidator.parse(req.body);
+  if(isPublished === false){
+    res.status(403);
+    throw new Error("You cannot unpublish a quiz");
+  }
+
   quiz.title = title || quiz.title;
   quiz.isPublished = isPublished || quiz.isPublished;
-  await quiz.save();
+
+  //save quiz to db and delete redis cache
+  await Promise.all([quiz.save(), deleteRedisQuiz(quiz.id)]);
 
   const { __v, author, id: _, ...data } = quiz.toJSON();
   res.status(200).json({ data });
